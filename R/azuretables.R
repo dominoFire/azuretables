792e62c76464908a61e624df1f0157681c0e4c75
dominofire <fer.aguilar.reyes@gmail.com>
2015-04-08 01:02:30 -0500

    Query entities complete.

--- R/azuretables.R
+++ R/azuretables.R
@@ -1,9 +1,8 @@
 library(RCurl)
 library(digest)
 library(XML)
-
-storage_account = 'tablaprueba'
-storage_key = 'qkGxxfSCq8VTF6bB1Lmz5zajIB99qCKt3JL5vgs53FjdElducNSqqylnpliC1hRpwNoD1eA9FMHxu0DbT0daoQ=='
+#library(R.utils)
+#library(plyr)
 
 # returns string w/o leading or trailing whitespace
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
@@ -11,25 +10,22 @@ trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 table_hostname = function(storage_account) 
   sprintf('%s.table.core.windows.net', storage_account)
 
-
 # Shared Key Lite auth
-sign_table_request <- function(date, canonicalized_resource) {
-  string_to_sign = sprintf('%s\n%s', date, canonicalized_resource)
-  key_decoded = base64Decode(storage_key)
-  signed = hmac(key=key_decoded, object=enc2utf8(string_to_sign), algo='sha256', raw=T)
-  ret = sprintf('SharedKeyLite %s:%s', storage_account, base64Encode(signed)[[1]])
-  return(ret)
-}
+shared_key_lite_table = function(date, canonicalized_resource)
+  sprintf('%s\n%s', date, canonicalized_resource)
 
-sign_table <- function(http_verb, content_md5, content_type, date, canonicalized_resource) {
-  string_to_sign = paste(http_verb, content_md5, content_type, date, canonicalized_resource, sep='\n', collapse='')
+# Shared Key auth
+shared_key_table <- function(http_verb, content_md5, content_type, date, canonicalized_resource)
+  paste(http_verb, content_md5, content_type, date, canonicalized_resource, sep='\n', collapse='')
+
+# Signs the request
+sign_request <- function(auth_mode, storage_account, string_to_sign) {
   key_decoded = base64Decode(storage_key)[[1]]
-  print(string_to_sign)
+  #print(string_to_sign)
   signed = hmac(key_decoded, enc2utf8(string_to_sign), algo='sha256', raw=T)
-  ret = sprintf('SharedKey %s:%s', storage_account, base64Encode(signed)[[1]])
+  ret = sprintf('%s %s:%s', auth_mode, storage_account, base64Encode(signed)[[1]])
   return(ret)
 }
-
 
 canonicalized_header_string = function(http_header) {
   # 1. Retrieve all headers for the resource that begin with x-ms-, including the x-ms-date header.
@@ -49,7 +45,7 @@ canonicalized_header_string = function(http_header) {
   # Construct the CanonicalizedHeaders string by concatenating all headers in this list into a single string.
   ret_string = trim(ret_string)
   # Add final \n
-  ret_string = sprintf('%s\n', ret_string)
+  ret_string = sprintf('%s', ret_string)
   return(ret_string)
 }
 
@@ -77,26 +73,83 @@ canonicalized_resource_string = function(storage_account, request_url) {
   ret = sprintf('%s%s', ret, qp_string)
   return(ret)
 }
-
   
-list_tables <- function() {
+list_tables <- function(storage_account, storage_key) {
   table_url = sprintf('https://%s/Tables', table_hostname(storage_account))
   http_verb = 'GET'
   gmt_date = format(Sys.time(), tz='GMT', format='%a, %d %b %Y %H:%M:%S GMT')
   http_header = c(Authorization=':D', 
                   `x-ms-date`=gmt_date,
-                  `x-ms-version`='2009-09-19',
+                  `x-ms-version`='2014-02-14',
                   Date=gmt_date,
                   DataServiceVersion='1.0;NetFx',
                   Accept='application/atom+xml')
-  auth = sign_table(http_verb = http_verb, 
-                    content_md5 = '', 
-                    content_type = '', 
-                    date = http_header['x-ms-date'], 
-                    canonicalized_resource = canonicalized_resource_string(storage_account, table_url))
+  auth = sign_request(auth_mode = 'SharedKey',
+                      storage_account = storage_account,
+                      shared_key_table(http_verb = http_verb, 
+                                       content_md5 = '', 
+                                       content_type = '', 
+                                       date = http_header['x-ms-date'], 
+                                       canonicalized_resource = canonicalized_resource_string(storage_account, table_url)))
   http_header['Authorization'] = auth
-  x = RCurl::getURL(url = table_url, .opts = list(customrequest=http_verb, verbose=T, httpheader=http_header))
-  print(x)
+  body = RCurl::getURL(url = table_url, .opts = list(customrequest=http_verb, httpheader=http_header))
+  doc = xmlTreeParse(body)
+  root = xmlRoot(doc)
+  R.utils::captureOutput({
+    table_names = sapply(getNodeSet(root, '//d:TableName'), xmlValue)
+  })
+  return(table_names)
 }
 
-list_tables()
+query_entities = function(storage_account, storage_key, table_name, include_azure_columns=FALSE) {
+  table_url = sprintf('https://%s/%s()', table_hostname(storage_account), table_name)
+  http_verb = 'GET'
+  gmt_date = format(Sys.time(), tz='GMT', format='%a, %d %b %Y %H:%M:%S GMT')
+  http_header = c(Authorization=':D', 
+                  `x-ms-date`=gmt_date,
+                  `x-ms-version`='2014-02-14',
+                  Date=gmt_date,
+                  DataServiceVersion='1.0;NetFx',
+                  Accept='application/atom+xml')
+  auth = sign_request(auth_mode = 'SharedKey',
+                      storage_account = storage_account,
+                      shared_key_table(http_verb = http_verb, 
+                                       content_md5 = '', 
+                                       content_type = '', 
+                                       date = http_header['x-ms-date'], 
+                                       canonicalized_resource = canonicalized_resource_string(storage_account, table_url)))
+  http_header['Authorization'] = auth
+  h = basicHeaderGatherer()
+  body = RCurl::getURL(url = table_url, 
+                       .opts = list(customrequest=http_verb, httpheader=http_header),
+                       headerfunction = h$update)
+  str(h$value())
+  doc = xmlTreeParse(body)
+  root = xmlRoot(doc)
+  R.utils::captureOutput({ #Avoid annoying error messages
+    entries = getNodeSet(root, "//*[starts-with(local-name(), 'entry')]")
+    rows = lapply(entries, function(x) xmlChildren(xmlChildren(x)$content)[['m:properties']])
+    values = lapply(rows, function(x) 
+      unlist(sapply(xmlChildren(x), 
+                    function(x) ifelse(length(x)==0, NA, xmlValue(x)) )))
+  })
+  df = data.frame(Reduce(rbind, values), stringsAsFactors = F, row.names=1:length(values))
+  colnames(df) = gsub('^d.', '',  colnames(df))
+  azure_colnames = c('PartitionKey', 'RowKey', 'Timestamp')
+  if(!include_azure_columns & any(colnames(df) %in% azure_colnames))
+    df = df[, !(colnames(df) %in% azure_colnames)]
+  df = plyr::colwise(type.convert, as.is=T, na.strings=c(''))(df)
+  #print(root)
+  #write.table(body, file='test.txt')
+  return(df)
+}
+
+#Test
+storage_account = 'kuboliac'
+storage_key = 'pzRK4LP/y1DlsO54tSyNa7TM9ex2O+FnklS1Xj3foQ/3rrDi/OynMb0yDjTDnekqdsoP2w5NcBCnKe/8Qas4Ug=='
+
+table_names = list_tables(storage_account, storage_key)
+print(table_names)
+az_table = query_entities(storage_account, storage_key, 'gnreference')
+print(head(az_table))
+str(az_table)
